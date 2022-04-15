cf5d0f4ffd0e850cd523b8cb2116620153d41464
Karolina <lukierek82@wp.pl>
2022-04-15 10:14:22 +0200

    feature: modify sex mapping

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
-train_data = pd.read_csv(
-    "C:/Users/User/Desktop/infa materialy/SEM 6/BIAI/BIAI-Neural-Networks-Project/data/train.csv")  # loading the data
-test_data = pd.read_csv(
-    "C:/Users/User/Desktop/infa materialy/SEM 6/BIAI/BIAI-Neural-Networks-Project/data/test.csv")  # ('../data/test.csv')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+train_data = pd.read_csv("data/train.csv")  # loading the data
+test_data = pd.read_csv("data/test.csv")  # ('../data/test.csv')
 
 # Train data has column: "Survived", test data doesn't
 
@@ -23,20 +24,22 @@ print("Total number of rows in test data ", test_data.shape[0])
 # Getting rid of 'Cabin' column as it is not of significance to survival
 train_data.drop('Cabin', axis=1, inplace=True)
 test_data.drop('Cabin', axis=1, inplace=True)
-# Age column has several missing values, so they will be filled with mean
-for data in train_data:
-    data.Age.fillna(data.Age.mean(), inplace=True)
-for data in test_data:
-    data.Age.fillna(data.Age.mean(), inplace=True)
 
+# Age column has several missing values, so they will be filled with mean
+train_data.Age.fillna(train_data.Age.mean(), inplace=True)
+test_data.Age.fillna(test_data.Age.mean(), inplace=True)
+
+# Age column mean
+print(train_data.Age.mean())
+print(test_data.Age.mean())
 
 # Changing string values of gender to categorical ones
-def change_gender(x):
-    if x == 'male':
-        return 0
-    elif x == 'female':
-        return 1
+train_data.Sex = train_data.Sex.map({'female': 1, 'male': 0})
+test_data.Sex = test_data.Sex.map({'female': 1, 'male': 0})
 
+# Rounding the values in Age column
+train_data.Age = train_data.Age.apply(np.ceil)
+test_data.Age = test_data.Age.apply(np.ceil)
 
-train_data.Sex = train_data.Sex.apply(change_gender)
-test_data.Sex = test_data.Sex.apply(change_gender)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(test_data)
