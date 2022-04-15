65e10091448bdf93344f1ffcc1d7611bc8f0b23f
Karolina <lukierek82@wp.pl>
2022-04-15 11:20:45 +0200

    file: add file split

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 import os
+import pandas as pd
+import src.data_clean.clean as dc
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+# Train data has column: "Survived", test data doesn't
 train_data = pd.read_csv("data/train.csv")  # loading the data
 test_data = pd.read_csv("data/test.csv")  # ('../data/test.csv')
-
-# Train data has column: "Survived", test data doesn't
 
 train_data.head()
 print("Total number of rows in training data: ", train_data.shape[0])
@@ -19,27 +16,8 @@ print("Total number of rows in test data: ", test_data.shape[0])
 ((train_data.groupby(['Sex', 'Survived']).Survived.count() * 100) / train_data.groupby('Sex').Survived.count())
 # The result is that 74% of women 18% of men survived
 
-# Handling missing values:
-
-# Getting rid of 'Cabin' column as it is not of significance to survival
-train_data.drop(columns=['PassengerId', 'Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'], inplace=True)
-test_data.drop(columns=['PassengerId', 'Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'], inplace=True)
-
-# Age column has several missing values, so they will be filled with mean
-train_data.Age.fillna(train_data.Age.mean(), inplace=True)
-test_data.Age.fillna(test_data.Age.mean(), inplace=True)
-
-# Age column mean
-print("Age column mean in training data: ", train_data.Age.mean())
-print("Age column mean in test data: ", test_data.Age.mean())
-
-# Changing string values of gender to categorical ones
-train_data.Sex = train_data.Sex.map({'female': 1, 'male': 0})
-test_data.Sex = test_data.Sex.map({'female': 1, 'male': 0})
-
-# Rounding the values in Age column
-train_data.Age = train_data.Age.apply(np.ceil)
-test_data.Age = test_data.Age.apply(np.ceil)
+# Cleaning and handling missing values in data sets:
+train_data, test_data = dc.clean_data_sets(train_data, test_data)
 
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     print(train_data)
---
+++ src/data_clean/clean.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def clean_data_sets(train_data_set, test_data_set):
+    # Getting rid of columns that are not significant to survival
+    train_data_set.drop(columns=['PassengerId', 'Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'],
+                        inplace=True)
+    test_data_set.drop(columns=['PassengerId', 'Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'],
+                       inplace=True)
+
+    # Age column has several missing values, so they will be filled with mean
+    train_data_set.Age.fillna(train_data_set.Age.mean(), inplace=True)
+    test_data_set.Age.fillna(test_data_set.Age.mean(), inplace=True)
+
+    # Age column mean
+    print("Age column mean in training data: ", train_data_set.Age.mean())
+    print("Age column mean in test data: ", test_data_set.Age.mean())
+
+    # Changing string values of gender to categorical ones
+    train_data_set.Sex = train_data_set.Sex.map({'female': 1, 'male': 0})
+    test_data_set.Sex = test_data_set.Sex.map({'female': 1, 'male': 0})
+
+    # Rounding values in Age column
+    train_data_set.Age = train_data_set.Age.apply(np.ceil)
+    test_data_set.Age = test_data_set.Age.apply(np.ceil)
+    return train_data_set, test_data_set
