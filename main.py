d45686d8d5f61855f0218717e24f0ae3efe5d765
Karolina <lukierek82@wp.pl>
2022-04-15 10:49:30 +0200

    feature: add drop columns for the first model

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ test_data = pd.read_csv("data/test.csv")  # ('../data/test.csv')
 # Train data has column: "Survived", test data doesn't
 
 train_data.head()
-print("Total number of rows in training data ", train_data.shape[0])
-print("Total number of rows in test data ", test_data.shape[0])
+print("Total number of rows in training data: ", train_data.shape[0])
+print("Total number of rows in test data: ", test_data.shape[0])
 
 # Statistics of survival between genders
 ((train_data.groupby(['Sex', 'Survived']).Survived.count() * 100) / train_data.groupby('Sex').Survived.count())
@@ -22,16 +22,16 @@ print("Total number of rows in test data ", test_data.shape[0])
 # Handling missing values:
 
 # Getting rid of 'Cabin' column as it is not of significance to survival
-train_data.drop('Cabin', axis=1, inplace=True)
-test_data.drop('Cabin', axis=1, inplace=True)
+train_data.drop(columns=['PassengerId', 'Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'], inplace=True)
+test_data.drop(columns=['PassengerId', 'Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'], inplace=True)
 
 # Age column has several missing values, so they will be filled with mean
 train_data.Age.fillna(train_data.Age.mean(), inplace=True)
 test_data.Age.fillna(test_data.Age.mean(), inplace=True)
 
 # Age column mean
-print(train_data.Age.mean())
-print(test_data.Age.mean())
+print("Age column mean in training data: ", train_data.Age.mean())
+print("Age column mean in test data: ", test_data.Age.mean())
 
 # Changing string values of gender to categorical ones
 train_data.Sex = train_data.Sex.map({'female': 1, 'male': 0})
@@ -42,4 +42,4 @@ train_data.Age = train_data.Age.apply(np.ceil)
 test_data.Age = test_data.Age.apply(np.ceil)
 
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print(test_data)
+    print(train_data)
