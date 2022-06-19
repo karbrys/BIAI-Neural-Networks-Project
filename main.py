de4db4aeadf59078c8e9946e1b218be278a9cb7f
Karolina Bryś <karolina.brys@itsilesia.com>
2022-06-19 17:55:40 +0200

    feature: clean code, update models, add validation

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from tensorflow.python.keras import layers
 import src.data_clean.clean as dc
 import src.model.model as md
 import matplotlib.pyplot as plt
-import stats.histograms as sth
+import stats.plots as sth
 import seaborn as sns
 from sklearn import metrics
 
@@ -16,25 +16,28 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # Train data has column: "Survived", test data doesn't
 train_data = pd.read_csv("data/train.csv")  # loading the data
 test_data = pd.read_csv("data/test.csv")  # ('../data/test.csv')
+validate_data = pd.read_csv("data/validate.csv")  # ('../data/validate.csv')
 
 train_data.head()
 print("Total number of rows in training data: ", train_data.shape[0])
 print("Total number of rows in test data: ", test_data.shape[0])
+print("Total number of rows in validate data: ", validate_data.shape[0])
 
 # Statistics of survival between genders
 ((train_data.groupby(['Sex', 'Survived']).Survived.count() * 100) / train_data.groupby('Sex').Survived.count())
 # The result is that 74% of women 18% of men survived
 
 # Cleaning and handling missing values in data sets:
-train_data_clean, test_data_clean = dc.clean_data_sets(train_data, test_data)
+train_data_clean, test_data_clean, validate_data_clean = dc.clean_data_sets(train_data, test_data, validate_data)
 
 # Histograms
-#sth.create_survival_age_histogram(train_data_clean)
-#sth.create_survival_pclass_histogram(train_data_clean)
-#sth.create_correlation(train_data_clean)
+# sth.create_survival_age_histogram(train_data_clean)
+# sth.create_survival_pclass_histogram(train_data_clean)
+# sth.create_correlation(train_data_clean)
 
 # Take out target column - in our case survived column
 target = train_data_clean.pop('Survived')
+validate_target = validate_data_clean.pop('Survived')
 
 # Converting pandas data sets to tensorflow data sets
 train_data_clean_tf = tf.convert_to_tensor(train_data_clean)
@@ -45,35 +48,42 @@ normalizer = tf.keras.layers.Normalization(axis=-1)
 normalizer.adapt(train_data_clean)
 
 # Creating model
-model = md.create_model_6(normalizer)
+model = md.create_model_11(normalizer)
 
 # Fitting model
-history = model.fit(train_data_clean, target,batch_size=32, verbose=2, epochs=200) #combinations also with:batch_size=64 epochs=100
+history = model.fit(train_data_clean, target, batch_size=32, verbose=2,
+                    epochs=200)  # combinations also with:batch_size=64 epochs=100
 
 # Plotting the Loss and Accuracy over 250 epochs
-#pd.DataFrame(history.history).plot(title="Loss and Accuracy")
-#plt.savefig('plot11a.png')
-#plt.show()
+# pd.DataFrame(history.history).plot(title="Loss and Accuracy")
+# plt.savefig('plot11a.png')
+# plt.show()
 
 # Model summary
 model.summary()
 
+# Evaluation
+print(model.evaluate(validate_data_clean, validate_target))
+
 # Prediction for test data
-predict = model.predict(train_data_clean)
+predict = model.predict(test_data_clean)
 predict = (predict > 0.5).astype(int).ravel()
 print(predict)
 
-# Evaluation
-model.evaluate(train_data_clean, target)
-
 # Submission to csv
-#result = pd.DataFrame({"Pclass": test_data_clean.Pclass, "Sex": test_data_clean.Sex, "Age": test_data_clean.Age, "Survived": predict})
-#result.to_csv("final_result_m11a.csv", index=False)
+# result = pd.DataFrame({"Pclass": test_data_clean.Pclass, "Sex": test_data_clean.Sex, "Age": test_data_clean.Age, "Survived": predict})
+# result.to_csv("final_result_m11a.csv", index=False)
+
 
 # Plotting the confusion matrix in heatmap
-matrix = metrics.confusion_matrix(target, predict)
-sns.heatmap(matrix, annot = True,fmt = 'g')
-plt.savefig('confusion6.png')
-plt.show()
+# matrix = metrics.confusion_matrix(target, predict)
+# sns.heatmap(matrix, annot = True,fmt = 'g')
+# plt.savefig('confusion.png')
+# plt.show()
 
-
+# Reading predictions
+# df = pd.read_csv("training_results/final_result_m1.csv")
+# count = (df['Survived'] == 1).sum()
+# print('One: ', count)
+# count = (df['Survived'] == 0).sum()
+# print('Zero: ', count)
--- src/model/model.py
+++ src/model/model.py
@@ -5,14 +5,15 @@ from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.models import Sequential, load_model
 
 
-def create_model(normalizer):
+def create_model_1(normalizer):
     # Creating model layers
     model = tf.keras.Sequential([
-            normalizer,
-            tf.keras.layers.Dense(10, activation='relu'),
-            tf.keras.layers.Dense(10, activation='relu'),
-            tf.keras.layers.Dense(1)
-        ])
+        normalizer,
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(8, activation='relu'),
+        tf.keras.layers.Dense(6, activation='relu'),
+        tf.keras.layers.Dense(1)
+    ])
 
     # Compiling model
     model.compile(optimizer='adam',
@@ -20,126 +21,137 @@ def create_model(normalizer):
                   metrics=['accuracy'])
     return model
 
+
 # GOOD
-def create_model2(normalizer):
+def create_model_2(normalizer):
     # Creating model layers
     model2 = tf.keras.Sequential([
-            normalizer,
-            tf.keras.layers.Dense(10, activation='sigmoid'),
-            tf.keras.layers.Dense(10, activation='sigmoid'),
-            tf.keras.layers.Dense(1)
-        ])
+        normalizer,
+        tf.keras.layers.Dense(20, activation='sigmoid'),
+        tf.keras.layers.Dense(16, activation='relu'),
+        tf.keras.layers.Dense(12, activation='relu'),
+        tf.keras.layers.Dense(8, activation='sigmoid'),
+        tf.keras.layers.Dense(1)
+    ])
 
     # Compiling model
     model2.compile(optimizer='adam',
-                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
+                   loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                   metrics=['accuracy'])
     return model2
 
-def create_model3(normalizer):
+
+def create_model_3(normalizer):
     # Creating model layers
     model3 = tf.keras.Sequential([
-            normalizer,
-            tf.keras.layers.Dense(10, activation='sigmoid'),
-            tf.keras.layers.Dense(10, activation='relu'),
-            tf.keras.layers.Dense(1)
-        ])
+        normalizer,
+        tf.keras.layers.Dense(10, activation='sigmoid'),
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(1)
+    ])
 
     # Compiling model
     model3.compile(optimizer='adam',
-                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
+                   loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                   metrics=['accuracy'])
     return model3
 
-def create_model4(normalizer):
+
+def create_model_4(normalizer):
     # Creating model layers
     model4 = tf.keras.Sequential([
-            normalizer,
-            tf.keras.layers.Dense(10, activation='relu'),
-            tf.keras.layers.Dense(10, activation='sigmoid'),
-            tf.keras.layers.Dense(1)
-        ])
+        normalizer,
+        tf.keras.layers.Dense(12, activation='relu'),
+        tf.keras.layers.Dense(8, activation='sigmoid'),
+        tf.keras.layers.Dense(4, activation='sigmoid'),
+        tf.keras.layers.Dense(1)
+    ])
 
     # Compiling model
     model4.compile(optimizer='adam',
-                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
+                   loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                   metrics=['accuracy'])
     return model4
 
-def create_model5(normalizer):
+
+def create_model_5(normalizer):
     # Creating model layers
     model5 = tf.keras.Sequential([
-            normalizer,
-            tf.keras.layers.Dense(10, activation='relu'),
-            tf.keras.layers.Dense(10, activation='relu'),
-            tf.keras.layers.Dense(1)
-        ])
+        normalizer,
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(1,  activation='relu')
+    ])
 
     # Compiling model
     model5.compile(optimizer='adam',
-                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
+                   loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                   metrics=['accuracy'])
     return model5
 
 
 def create_model_6(normalizer):
     # Creating model layers
     model6 = tf.keras.Sequential([
-            normalizer,
-            tf.keras.layers.Dense(10, activation='relu'),
-            tf.keras.layers.Dense(10, activation='relu'),
-            tf.keras.layers.Dense(10, activation='relu'),
-            tf.keras.layers.Dense(1)
-        ])
+        normalizer,
+        tf.keras.layers.Dense(30, activation='relu'),
+        tf.keras.layers.Dense(20, activation='relu'),
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(1)
+    ])
 
     # Compiling model
     model6.compile(optimizer='adam',
-                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
+                   loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                   metrics=['accuracy'])
     return model6
+
+
+def create_model_7(normalizer):
+    # Creating model layers
+    model7 = tf.keras.Sequential([
+        normalizer,
+        tf.keras.layers.Dense(10, activation='tanh'),
+        tf.keras.layers.Dense(10, activation='tanh'),
+        tf.keras.layers.Dense(1)
+    ])
+
+    # Compiling model
+    model7.compile(optimizer='adam',
+                   loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                   metrics=['accuracy'])
+    return model7
+
+
+def create_model_8(normalizer):
+    # Creating model layers
+    model8 = tf.keras.Sequential([
+        normalizer,
+        tf.keras.layers.Dense(15, activation='softplus'),
+        tf.keras.layers.Dense(10, activation='softplus'),
+        tf.keras.layers.Dense(5, activation='softplus'),
+        tf.keras.layers.Dense(1)
+    ])
+
+    # Compiling model
+    model8.compile(optimizer='adam',
+                    loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                    metrics=['accuracy'])
+    return model8
+
 
 def create_model_9(normalizer):
     # Creating model layers
     model9 = tf.keras.Sequential([
-            normalizer,
-            tf.keras.layers.Dense(10, activation='tanh'),
-            tf.keras.layers.Dense(10, activation='tanh'),
-            tf.keras.layers.Dense(1)
-        ])
+        normalizer,
+        tf.keras.layers.Dense(12, activation='relu'),
+        tf.keras.layers.Dense(8, activation='relu'),
+        tf.keras.layers.Dense(4, activation='relu'),
+        tf.keras.layers.Dense(1, activation="sigmoid")
+    ])
 
     # Compiling model
-    model9.compile(optimizer='adam',
-                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-    return model9
-
-def create_model_10(normalizer):
-    # Creating model layers
-    model10 = tf.keras.Sequential([
-            normalizer,
-            tf.keras.layers.Dense(10, activation='softplus'),
-            tf.keras.layers.Dense(10, activation='softplus'),
-            tf.keras.layers.Dense(1)
-        ])
-
-    # Compiling model
-    model10.compile(optimizer='adam',
-                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-    return model10
-
-def create_model_11(normalizer):
-    # Creating model layers
-    model = tf.keras.Sequential([
-            normalizer,
-            tf.keras.layers.Dense(12, activation='relu'),
-            tf.keras.layers.Dense(8, activation='relu'),
-            tf.keras.layers.Dense(4, activation='relu'),
-            tf.keras.layers.Dense(1, activation="sigmoid")
-        ])
-
-    # Compiling model
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
+    model9.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
                   loss=tf.keras.losses.BinaryCrossentropy(),
                   metrics=['accuracy'])
-    return model
+    return model9
